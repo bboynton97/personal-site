b713babd0652f00928455860c618492e51f9e7f7
Braelyn Boynton <bboynton97@gmail.com>
2025-08-18 17:31:45 +0200

    made home page not terrible

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { PersonalComponent } from './personal/personal.component';
+import { WorkComponent } from './work/work.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'personal', component: PersonalComponent },
+  { path: 'work', component: WorkComponent },
+  { path: '**', redirectTo: '' }
+];
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { siInstagram, siX } from 'simple-icons';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  readonly instagramIcon = { path: siInstagram.path, title: siInstagram.title };
+  readonly xIcon = { path: siX.path, title: siX.title };
 }
