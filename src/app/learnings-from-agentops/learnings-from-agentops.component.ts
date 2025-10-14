f46724df0515fd3f01efa01dd58e83ac735327e1
Braelyn Boynton <bboynton97@gmail.com>
2025-10-13 22:55:36 -0700

    resume

---
+++ src/app/learnings-from-agentops/learnings-from-agentops.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learnings-from-agentops',
+  standalone: true,
+  imports: [],
+  templateUrl: './learnings-from-agentops.component.html',
+  styleUrl: './learnings-from-agentops.component.css'
+})
+export class LearningsFromAgentopsComponent {
+
+}
